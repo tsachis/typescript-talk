ea2c6317fc472556d6f56a8420629af0c32fdad5
tshushan <tshushan@outbrain.com>
2022-07-12 12:51:47 +0300

    more

--- 4-generics.ts
+++ 4-generics.ts
@@ -28,6 +28,18 @@ const arr = myMap([1,2,3], (item) => item.toString())
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 function myMap(arr, mapFunc) {
   return arr.reduce((acc, curr) => [...acc, mapFunc(curr)], []);
 }
